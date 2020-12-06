0b81d19ffae12d60cc374e1449f55093fbdd0743
italosilva98 <italosilvestre3@icloud.com>
2020-12-06 17:44:39 -0300

    Deploy da aplicação

--- core/settings.py
+++ core/settings.py
@@ -12,13 +12,14 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 
 from pathlib import Path
 from decouple import config, Csv
-
+import django_heroku
 import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 
+django_heroku.settings(locals())
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
